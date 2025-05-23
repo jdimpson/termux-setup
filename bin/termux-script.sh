5b61e846b1ef3a72ffac6af2a3bac022c750ed44
Jeremy D. Impson <github@echo3.info>
2025-05-23 15:50:50 -0400

    initial

---
+++ bin/termux-script.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# use this if you want to use ssh or scp
+. $HOME/bin/my-source-ssh-agent
+#export SSH_AUTH_SOCK=/data/data/com.termux/files/usr/var/run/ssh-agent
+
+echo 'Example script. Press Enter to continue.'
